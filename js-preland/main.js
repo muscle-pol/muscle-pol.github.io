50c11ca22510e1028a6918114dcaebe37e2b57a4
Your Name <you@example.com>
2021-11-29 22:19:23 +0300

    added

---
+++ js-preland/main.js
@@ -0,0 +1,17 @@
+
+var swiper = new Swiper(".swiper-container", {
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	  },
+});
+(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+ym(85852604, "init", {
+    clickmap:true,
+    trackLinks:true,
+    accurateTrackBounce:true,
+    webvisor:true
+});
